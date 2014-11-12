3d438a9796d8387b7665498f5ea781d77c605dd8
Evan Wills <evan.wills@acu.edu.au>
2014-11-13 08:32:17 +1100

    updated evans_filter.js did some tweaking.

--- evans_filter.js
+++ evans_filter.js
@@ -4,7 +4,7 @@
 /**
  * @function primaryFilterBySelect() shows or hides 
  *
- * @param string filterField the CSS selector (usually ID) for the
+ * @param string filterID the CSS selector (usually ID) for the
  *        select field to be used as the filter
  *
  * @param string filterWrapper the CSS selector for the wrapper of
@@ -21,13 +21,7 @@
  *        extra stuff based on filter values pass in a function to
  *        handle it with a single string param
  */
-$.primaryFilterBySelect = function( filterField , filterWrapper , filterItem , secondaryFilterIDs , postFilterFunction ) {
-
-
-    var formFieldDetails = $._getFormFieldDetails(filterField);
-    if( formFieldDetails === false ) {
-        return false;
-    }
+$.primaryFilterBySelect = function( filterID , filterWrapper , filterItem , secondaryFilterIDs , postFilterFunction ) {
 
     // validate checkboxIds value
     try {
@@ -73,10 +67,10 @@ $.primaryFilterBySelect = function( filterField , filterWrapper , filterItem , s
         triggerSecondaryFilter = function() {}
     }
 
+    console.log($.getFormFieldType(filterID));
 
-
-    if( $(filterField).length == 0 ) {
-        console.log('could not find anything to use as a filter using the selector: "' + filterField + '"' );
+    if( $(filterID).length == 0 ) {
+        console.log('could not find anything to use as a filter using the selector: "' + filterID + '"' );
         return;
     } else {
         if( $(filterWrapper).length == 0 ) {
@@ -96,12 +90,12 @@ $.primaryFilterBySelect = function( filterField , filterWrapper , filterItem , s
          *           the filter show class and adds a data attribute
          *           (data-visible="true")
          */
-        $(formFieldDetails.baseSelector).on('change',function(e){
+        $(filterID).on('change',function(e){
             /**
              * @var array list of class names to be filtered on from the
              *      selected option's value
              */
-            var filterClasses = $._splitValueOnWhiteSpace( $(formFieldDetails.valueSelector).val() );
+            var filterClasses = $(filterID + ' option:selected').val().split(' ');
 
             // reset filter so nothing is shown
             $( selector ).removeClass('filter-show').addClass('filter-hide').attr('data-visible',false);
@@ -145,10 +139,8 @@ $.primaryFilterBySelect = function( filterField , filterWrapper , filterItem , s
  *       which items can and cannot be made visible. It will not work on its
  *       own unless the data attribute is rendered in the served HTML
  *
- * @param string filterField the CSS selector (usually ID) for the
+ * @param string filterID the CSS selector (usually ID) for the
  *        checkbox field to be used as the filter
- *          NOTE: if filter is a radio button list use the selector
- *                  input[name='radioname']:checked
  *
  * @param string filterWrapper the CSS selector for the wrapper of
  *        the items to be filtered
@@ -156,63 +148,60 @@ $.primaryFilterBySelect = function( filterField , filterWrapper , filterItem , s
  * @param string filterItem the element or class defining which items
  *        should be filtered
  */
-$.secondaryFilterByCheckbox = function( filterField , filterWrapper , filterItem ) {
+$.secondaryFilterByCheckbox = function( filterID , filterWrapper , filterItem ) {
 
-    var formFieldDetails = $._getFormFieldDetails(filterField);
-    if( formFieldDetails === false ) {
-        return false;
-    }
+    var filterType = $.getFormFieldType(filterID)
 
+    if( filterType === )
     /**
-     * @var array list of class names to be filtered on from the
-     *      selected option's value
+     * @var string filterVal the value of the checkbox filter
      */
-    var filterClasses = $._splitValueOnWhiteSpace( $( formFieldDetails.valueSelector ).val() );
+    var filterVal = $(filterID).val();
 
-    if( filterClasses.length < 0 ) {
-        if( formFieldDetails.fieldType === 'checkbox' ) {
-            $( formFieldDetails.baseSelector ).on('change',function(e){
+    console.log(;);
 
-                var selector = filterWrapper + ' ' + filterItem + '.' + filterClass[0];console.log(selector);
+    if( filterVal != '' ) {
+        $(filterID).on('change',function(e){
 
-                if( $( formFieldDetails.valueSelector ).is(':checked') == false ) {
-                        // find all the visible items that match the secondary filter
-                        $( selector ).each(function(){
-                            // step through each matched item
-                            // check if it was matched by the primary filter
-                            if( $(this).data('visible','true') && $(this).hasClass('filter-show') ) {
-                                // hide the item by removing the filter-show class and adding the filter-hide class
-                                $(this).addClass('filter-hide').removeClass('filter-show');
-                            }
-                        });
-                } else {
-                    // find all the visible items that match the secondary filter
-                   $( selector ).each(function(){
-                        // step through each matched item
-                        // check if it was matched by the primary filter
-                        if( $(this).data('visible','true') && $(this).hasClass('filter-show') ) {
-                            // show the item by adding the filter-show class and removing the filter-hide class
-                            $(this).addClass('filter-show').removeClass('filter-hide');
-                        }
-                    });
-                }
-            });
-        } else {
+            var selector = filterWrapper + ' ' + filterItem + '.' + $(filterID).val();
 
-            var selector = filterWrapper + ' ' + filterItem + '.';
-            $( formFieldDetails.baseSelector ).on('change',function(e){
-                for( var i = 0 ; i < filterClasses.length ; i += 1 ) {  
-                    $( selector + filterClasses[i] ).each(function() {
-                        // step through each matched item
-                        // check if it was matched by the primary filter
-                        if( $(this).data('visible','true') && $(this).hasClass('filter-hide') ) {
-                            // hide the item by removing the filter-show class and adding the filter-hide class
-                            $(this).addClass('filter-hide').removeClass('filter-hide');
-                        }
-                    });
-                }
-            });
-        }
+            if( $(filterID).is(':checked') == false ) {
+                // find all the visible items that match the secondary filter
+                $( selector ).each(function(){
+                    // step through each matched item
+                    // check if it was matched by the primary filter
+                    if( $(this).data('visible','true') ) {
+                        // hide the item by removing the filter-show class and adding the filter-hide class
+                        $(this).addClass('filter-hide').removeClass('filter-show');
+                    }
+                });
+            } else {
+                // find all the visible items that match the secondary filter
+                $( selector ).each(function(){
+                    // step through each matched item
+                    // check if it was matched by the primary filter
+                    if( $(this).data('visible','true') ) {
+                        // show the item by adding the filter-show class and removing the filter-hide class
+                        $(this).addClass('filter-show').removeClass('filter-hide');
+                    }
+                });
+            }
+        });
+    }
+}
+
+$.getFormFieldType = function( filterID ) {
+    var tagName = $(filterID).prop('tagName').toLowerCase();console.log(tagName);
+    if( tagName == 'input' ) {
+        console.log( $(filterID).attr('type') );
+        return $(filterID).attr('type').toLowerCase();
+    } else if ( tagName === 'textarea' ) {
+        return 'text';
+    } else if ( tagname === 'select' ) {
+        return tagName;
+    } else {
+        console.log('Supplied filter ID did not return a valid input type. ' + tagname + ' was returned. Was expecting, select; input or textarea');
+        return false;
     }
 }
 
@@ -222,23 +211,23 @@ $.secondaryFilterByCheckbox = function( filterField , filterWrapper , filterItem
  *           select field to find the option with the matching data
  *           key/value pair. If found, that option is preselected
  *
- * @param string filterField the CSS selector for the select field to be
+ * @param string filterID the CSS selector for the select field to be
  *        used as the filter
  *
  * @param string dataAttr the data attribute name used and get variable
  *        used to store the value to be matched
  */
-$.presetFilterFromUrl = function( filterField , dataAttr ) {
+$.presetFilterFromUrl = function( filterID , dataAttr ) {
 
     /**
      * @var string sPageUrl the GET string part of the URL
      */
-    var getString = window.location.search.substring(1);
+    var sPageURL = window.location.search.substring(1);
 
     /**
      * @var array getVars all of the GET variables in the url
      */
-    var getVars = getString.split('&');
+    var getVars = sPageURL.split('&');
 
     /**
      * @var boolean string dataAttrVal the value of the data Attribute
@@ -248,6 +237,7 @@ $.presetFilterFromUrl = function( filterField , dataAttr ) {
 
     // loop through the GET variables
     for(var i = 0; i < getVars.length; i++) {
+ 
         /**
          * @var array getVarParts individual GET variable split into
          *      its key/value pair parts
@@ -261,27 +251,19 @@ $.presetFilterFromUrl = function( filterField , dataAttr ) {
         }
     }
 
-    var formFieldDetails = $._getFormFieldDetails(filterField);
-
-    if( formFieldDetails === false ) {
-        return false;
-    }
+    console.log($.getFormFieldType(filterID));
 
     // we have a value to use for preselecting
     if( dataAttrVal !== false ) {
         // loop through each option
-        if( formFieldDetails.fieldType == 'select' || formFieldDetails.fieldType == 'checkbox' || formFieldDetails.fieldType == 'radio' ) {
-            $( formFieldDetails.itemSelector ).each(function() {
-                // check if the option has the appropriate data attribute
-                // containing with the correct value
-                if( $(this).data(dataAttr) == dataAttrVal ) {
-                    // YAY, we have a match. Make this option preselected
-                    $(this).attr(formFieldDetails.fieldChosen,formFieldDetails.fieldChosen);
-                }
-            });
-        } else {
-            $( formFieldDetails.itemSelector ).val(dataAttrVal);
-        }
+        $(filterID + ' option').each(function(){
+            // check if the option has the appropriate data attribute
+            // containing with the correct value
+            if( $(this).data(dataAttr) == dataAttrVal ) {
+                // YAY, we have a match. Make this option preselected
+                $(this).attr('selected','selected');
+            }
+        });
         return dataAttrVal;
     } else {
         return '';
@@ -289,113 +271,6 @@ $.presetFilterFromUrl = function( filterField , dataAttr ) {
 
 }
 
-
 $.bookmarkThisFilter = function() {
 
-}
-
-// ==================================================================
-// internal helper functions
-
-/**
- * @function _splitValueOnWhiteSpace() takes a string, trims white
- *           space from begining and end, converts multiple white
- *           space characters into single space characters then splits
- *           the string into an array
- *
- * @param string input the string to be split
- *
- * @return array a list of items that were separated by one or more
- *         white space characters
- */
-$._splitValueOnWhiteSpace = function( inputValue ) {
-    var regex = /\s+/;
-    return inputValue.trim().replace(regex,' ').split(' ');
-}
-
-/**
- * @function _getFormFieldDetails() find the element matched by a given
- *           ID and returns its input type
- *
- * @param string filterField the ID of a given form field
- *
- * @return string false form field type if ID belonged to a form
- *         field or FALSE if it did not
- */
-$._getFormFieldDetails = function( filterField ) {
-
-    try {
-        $(filterField).prop('tagName');
-    } catch(e) {
-        console.log('Supplied filter field selector (' + filterField + ') did not match anything in the DOM.'); 
-        return false;
-    }
-
-    /**
-     * @var object formFieldDetails list of info about the filter form
-     *      element
-     *
-     * @property string baseSelector The selector that finds the filter
-     *           form element
-     *
-     * @property string itemSelector the selector that selects which
-     *           item in the field is selected (used for select box and
-     *           radio buttons)
-     *
-     * @property string valueSelector The selector for the selected item
-     *           (used for select box and radio buttons)
-     *
-     * @property string fieldChosen the selector that finds which part
-     *           of the element is selected (used for select box and
-     *           radio buttons)
-     *
-     * @property string fieldType HTML element the filter form field is
-     */
-    var formFieldDetails = {
-        baseSelector: filterField,
-        itemSelector: filterField,
-        valueSelector: filterField,
-        fieldChosen: '',
-        fieldType: $(filterField).prop('tagName').toLowerCase()
-    }
-
-    switch( formFieldDetails.fieldType  ) {
-        case 'input':
-                formFieldDetails.fieldType = $(filterField).attr('type').toLowerCase();
-                switch( formFieldDetails.fieldType ) {
-
-                    case 'radio':
-                        var radioName = $(filterField).attr('name');
-                        formFieldDetails.baseSelector = 'input[name="' + radioName + '"]';
-                        formFieldDetails.itemSelector = formFieldDetails.baseSelector;
-                        formFieldDetails.valueSelector = formFieldDetails.baseSelector + ':checked';
-                        formFieldDetails.fieldChosen = 'checked';
-                        break;
-
-                    case 'checkbox':
-                        formFieldDetails.fieldChosen = 'checked';
-                        break;
-
-                    case 'text':
-                        break;
-                    default:
-                        formFieldDetails.fieldType = 'text';
-                        break;
-                }
-                break;
-        case 'textarea':
-                formFieldDetails.fieldType = 'text';
-                break;
-        case 'select':
-                formFieldDetails.itemSelector = output.baseSelector + ' option'
-                formFieldDetails.valueSelector = output.itemSelector + ':selected'
-                formFieldDetails.fieldChosen = 'selected';
-                break;
-        default:
-                console.log('Supplied filterField (' + filterField + ') did not return a valid form field type. ' + tagname + ' was returned. Was expecting, select; input or textarea');
-                formFieldDetails = false;
-                break;
-    }
-    console.log(output);
-    return output;
 }
