df2c64d9661b38edf4de3f68319a914906ff5b05
Evan Wills <evan.wills@acu.edu.au>
2014-12-10 07:40:57 +1100

    updated js/page.js; moved the chosen config code to inside the document.ready() call and added filterFairy calls for other test areas of the page.

--- js/page.js
+++ js/page.js
@@ -1,35 +1,24 @@
 "use strict";
 
-var config = {
-	'.chosen-select'		: {},
-	'.chosen-select-deselect'	: {allow_single_deselect:true},
-	'.chosen-select-no-single'	: {disable_search_threshold:10},
-	'.chosen-select-no-results'	: {no_results_text:'Oops, nothing found!'},
-	'.chosen-select-width'		: {width:"95%"}
-}
-for (var selector in config) {
-	$('#course-filter').chosen(config[selector]);
-}
-
 
 $('document').ready(function(){
 	/**
 	 * @function tabSelectFunction() shows desired tab based on filter
 	 *           class value
-	 *
 	 */
 	var tabSelectFunction = function() {
 
-		var filterStrings = $('#course-filter option:selected').val().replace(/\s+/,' ').split(' ');
-console.log(filterStrings);
-	
+		var filterStrings = $('#course-filter option:selected').val().replace(/\s+/,' ');
+		filterStrings = $.trim(filterStrings);
+		filterStrings = filterStrings.split(' ');
+
 		/**
 		 * @var RegExp reg regular expression for cleaning up the
 		 *      value to be used to find the ID of the correct tab
 		 *      to be shown
 		 */
 		var reg = new RegExp('^grp__((mon|tues|wednes|thrus|fri|satur|sun)day)$');
-	
+
 		for( var i = 0 ; i < filterStrings.length ; i += 1 ) {
 
 			if( filterStrings[i].match( reg ) ) {
@@ -45,38 +34,13 @@ console.log(filterStrings);
 		return false;
 	};
 
-/*
-	var fairyConfig = {
-		filterSubject : '.filter-this li',
-		filterFields : [ {
-			selector : '#course-filter',
-			exclusive : false,
-			preset : {
-				getName: 'course_code',
-				dataName: 'coursecode',
-//				attrName: '', // if attrName and dataName are both present then something will break
-//				getValue: 
-			}
-		}, {
-			selector : '#agecohort',
-			exclusive : false,
-			preset : {
-				getName: 'agecohort',
-//				dataName: '',
-//				attrName: '',
-//				getValue: ''
-			}
-		} ],
-		hideAllOnEmpty : true,
-		postFilterFunction = tabSelectFunction
-                
-	};
-*/
+	$.filterFairy('#table-filter');
+	$.filterFairy('#admin-list');
 	var fairy = $.filterFairy( '#filter-select' );
-	fairy.presetFilter( '#course-filter' , 'coursecode' , 'course_code' );
-	fairy.presetFilter( '#agecohort' , 'agecohort' );
+	fairy.presetFilter( '#course-filter' , 'course' , 'coursecode' );
+	fairy.presetFilter( '#agecohort' , 'cohort' , 'agecohort' );
 	fairy.setHideAllOnEmptyFilter(true);
-	
+
 	$('#course-filter').on('change',tabSelectFunction);
 
 	$('#oWeek-tabs a').click(function (e) {
@@ -88,4 +52,20 @@ console.log(filterStrings);
 
 	$('#course-filter').trigger('change');
 	$('#agecohort').trigger('change');
+
+
+
+	var config = {
+		'.chosen-select'		: {},
+		'.chosen-select-deselect'	: {allow_single_deselect:true},
+		'.chosen-select-no-single'	: {disable_search_threshold:10},
+		'.chosen-select-no-results'	: {no_results_text:'Oops, nothing found!'},
+		'.chosen-select-width'		: {width:"95%"}
+	}
+	for (var selector in config) {
+		$('#course-filter').chosen(config[selector]);
+	}
+
+
+	$('#course-filter').trigger('change');
 });
